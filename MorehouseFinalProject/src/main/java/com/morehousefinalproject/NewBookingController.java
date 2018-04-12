ae6d85784a7d344e6e6a45abc88024bc973b93a2
Brian Morehouse <Brian Morehouse@10.0.0.177>
2018-04-12 15:04:52 -0400

    checkpoint 2 commit

--- MorehouseFinalProject/src/main/java/com/morehousefinalproject/NewBookingController.java
+++ MorehouseFinalProject/src/main/java/com/morehousefinalproject/NewBookingController.java
@@ -7,6 +7,8 @@ package com.morehousefinalproject;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,16 +19,9 @@ import javax.servlet.http.HttpServletResponse;
  * @author Brian Morehouse
  */
 public class NewBookingController extends HttpServlet {
+    
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -40,7 +35,37 @@ public class NewBookingController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+                
+//        SQLiteConnection sqlCon= new SQLiteConnection();
+//        String insertStatement = "INSERT INTO StudyRoomBooking(UserEmail, StudyRoomID, Date, StartTime, EndTime)"
+//                + " VALUES('email','1', date, startTIme, endTime)";
+//        String sql = "INSERT INTO warehouses(name,capacity) VALUES(?,?)";
+//        try {
+//            String date = request.getParameter("Date");
+//            String location = request.getParameter("Location");
+//            String room = request.getParameter("RoomNumber");
+//            //Need to parse other studyRoom table to find studyroomID to put into studyRoomBooking table
+//            
+//            String startTime = request.getParameter("StartTime");
+//            String endTime = request.getParameter("endTime");
+//            
+//            
+//            Connection conn = sqlCon.connect();
+//            
+//            
+//            
+//            
+//            
+//            
+//            
+//            
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(NewBookingController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        
         this.getServletContext().getRequestDispatcher("/WEB-INF/NewBookingView.jsp").include(request, response);
+        
+        
     }
 
     
